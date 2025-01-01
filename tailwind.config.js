357cf7a9aec045c23af19edcae852a7ec8c28953
Jared <jacallantine@crimson.ua.edu>
2025-01-01 05:44:41 -0600

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
         'max-md': { max: '768px' }, 
         'low-md': {max: '500px'},
         '300-md': {max: '300px'},
+        '1300-md': {max: '1300px'},
         '1100-md': {max: '1100px'}, 
         '1000-md': {max: '1000px'}, 
         'h-md': {'raw' : '(max-height:700px)'},
