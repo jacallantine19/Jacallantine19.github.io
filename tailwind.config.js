51f67c6a0d5e0a57288e6cd6a1565704cb50c4c2
Jared <jacallantine@crimson.ua.edu>
2025-01-01 07:12:52 -0600

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
 
       screens: {
         // Define a max-width breakpoint
-        'max-md': { max: '768px' }, 
-        'low-md': {max: '500px'},
-        '300-md': {max: '300px'},
         '1300-md': {max: '1300px'},
         '1100-md': {max: '1100px'}, 
         '1000-md': {max: '1000px'}, 
+        'max-md': { max: '768px' }, 
+        'low-md': {max: '500px'},
+        '300-md': {max: '300px'},
         'h-md': {'raw' : '(max-height:700px)'},
         'h-ld': {'raw' : '(max-height:500px)'}
       },
